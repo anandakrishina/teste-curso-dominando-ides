9e4f0735638589bbbeff37a017790d05da5ca594
anandakrishina <anandakrishinar@gmail.com>
2022-05-28 10:14:17 -0300

    alteração para numero 21

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -3,7 +3,7 @@ package com.dio;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		int a = 2;
+		int a = 21;
 		int b = 3;
 		System.out.println("Hello World! " + (a + b));
 	}
